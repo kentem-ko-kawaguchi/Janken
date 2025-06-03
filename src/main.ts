7756b76d243c21a943cc1f565c2bf7234a96ebf2
kentem-ko-kawaguchi <ko-kawaguchi@kentem.co.jp>
2025-06-03 15:13:37 +0900

    Janken - cpuHandテストケースの作成

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+export const cpuHand = () => {
+  return true;
+}
+
+export const playerHand = (playerHandIndex : number) => {
+  return true;
+}
+
+export const fightResult = (cpuHandIndex : number,playerHandIndex : number) => {
+  return true;
+}
+
+export const countUpResult = (resultIndex : number) => {
+  return true;
+}
+
